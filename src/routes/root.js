456b3d80383b3fb49e8da1fc64537a3b6a2d8300
Biruk Berhanu <meetbirukberhanu@gmail.com>
2023-01-06 18:37:17 +0300

    configure the routes using the jsx Routes

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,65 +1,57 @@
 import React from "react"
-import { createBrowserRouter, Outlet } from "react-router-dom"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+} from "react-router-dom"
 import { Login } from "../pages/login"
 import { Signup } from "../pages/signup"
 import { ForgotPassword } from "../pages/resetPassword"
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div className="root">
-        <div className="menubar">This is the Menu bar</div>
-        <div>
-          <Outlet />
-        </div>
-      </div>
-    ),
-    children: [
-      {
-        path: "venue",
-        element: (
-          <div className="venue">
-            <div className="sidebar">This is the venue side bar</div>
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      {" "}
+      <Route
+        path="/"
+        element={
+          <div className="root">
+            <div className="menubar">This is the Menu bar</div>
             <div>
               <Outlet />
             </div>
           </div>
-        ),
-        children: [
-          {
-            path: "general",
-            element: <div>This is the general tab</div>,
-          },
-          {
-            path: "menu-manager",
-            element: <div>This is the menu-manager tab</div>,
-          },
-          {
-            path: "menu-deal",
-            element: <div>This is the menu-deal tab</div>,
-          },
-          {
-            path: "event",
-            element: <div>This is the event tab</div>,
-          },
-          {
-            path: "setting",
-            element: <div>This is the setting tab</div>,
-          },
-          {
-            path: "users",
-            element: <div>This is the users tab</div>,
-          },
-        ],
-      },
-      {
-        path: "user",
-        element: <div>This is the user page</div>,
-      },
-    ],
-  },
-  { path: "/login", element: <Login /> },
-  { path: "/signup", element: <Signup /> },
-  { path: "/forgot-password", element: <ForgotPassword /> },
-])
+        }
+      >
+        <Route
+          path="venue"
+          element={
+            <div className="venue">
+              <div className="sidebar">This is the venue side bar</div>
+              <div>
+                <Outlet />
+              </div>
+            </div>
+          }
+        >
+          <Route path="general" element={<div>This is the general tab</div>} />
+          <Route
+            path="menu-manager"
+            element={<div>This is the menu manager tab</div>}
+          />
+          <Route
+            path="menu-deal"
+            element={<div>This is the menu deal tab</div>}
+          />
+          <Route path="event" element={<div>This is the event tab</div>} />
+          <Route path="setting" element={<div>This is the setting tab</div>} />
+          <Route path="users" element={<div>This is the users tab</div>} />
+        </Route>
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+    </>
+  )
+)
